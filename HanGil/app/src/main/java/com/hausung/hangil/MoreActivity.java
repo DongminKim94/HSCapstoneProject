491c4f58c2e674135840588ef45164229d4b85f0
Eastsky94 <dongminmin94@gmail.com>
2020-04-11 14:50:45 +0900

    #2 각 xml과 class 파일들 연결 및 페이지 이동 기능 완료

---
+++ HanGil/app/src/main/java/com/hausung/hangil/MoreActivity.java
@@ -0,0 +1,4 @@
+package com.hausung.hangil;
+
+public class More {
+}
---
+++ HanGil/app/src/main/java/com/hausung/hangil/SeminarShowActivity.java
@@ -0,0 +1,14 @@
+package com.hausung.hangil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SemniarShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seminar)show;
+    }
+}
