0d001f9b6faac9995802167c994c6c5df3c94cb5
Eastsky94 <dongminmin94@gmail.com>
2020-04-11 12:48:27 +0900

    Push 잘되나 확인

--- HanGil/app/src/main/java/com/hausung/hangil/MapActivity.java
+++ HanGil/app/src/main/java/com/hausung/hangil/MapActivity.java
@@ -2,7 +2,10 @@ package com.hausung.hangil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Button toMyPage = (Button) findViewById(R.id.toMyPage);
+        toMyPage.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //MyPage로 가는 인텐트 생성
+                        Intent intent = new Intent(getApplication(), MyPageActivity.class);
+
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
 }
