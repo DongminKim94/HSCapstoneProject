f9d4c5e683aa3c7fc67fea06c839e47c94e88a12
jihyunleee <st1315@naver.com>
2020-04-13 03:55:33 +0900

    activitiy slide(swipe)

--- HanGil/app/src/main/java/com/hausung/hangil/MyPageSecondActivity.java
+++ HanGil/app/src/main/java/com/hausung/hangil/MyPageSecondActivity.java
@@ -8,12 +8,17 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
 
 public class MyPageSecondActivity extends AppCompatActivity {
+    private SlidrInterface slidr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_second);
+        //스와이프 코드
+        slidr = Slidr.attach(this);
 
         //로그아웃 및 로그인 페이지로 이동
         Button logoutAndToStartPage = (Button) findViewById(R.id.logoutAndToStart);
@@ -47,9 +52,11 @@ public class MyPageSecondActivity extends AppCompatActivity {
         deleteAndToStart.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
+                        ////회원탈퇴 대신 임시로 넣은 로그아웃 코드
+                        FirebaseAuth.getInstance().signOut();
+
                         //SeminarShowActivity로 가는 인텐트 생성
                         Intent intent = new Intent(getApplication(), StartActivity.class);
-                        //회원탈퇴 코드 구현
                         startActivity(intent);
                     }
                 }
